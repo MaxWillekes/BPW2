f16e9383682fb2be5147aeac0db39d6ed64d6044
MaxWillekes <max.willekes@student.hku.nl>
2019-06-01 14:04:53 +0200

    Some basics implemented

---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    public int health;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            health--;
+            if(health == 0)
+            {
+                gameObject.SetActive(false);
+            }   
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        gameObject.transform.position = new Vector3(Input.GetAxisRaw("Mouse X"), -4.55f, 0);
+    }
+}
